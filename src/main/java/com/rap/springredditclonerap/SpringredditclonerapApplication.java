a45400194ab08cd8bfe8afb22cc81904ab76bc6f
rakaadli <34180704+rakaadli@users.noreply.github.com>
2021-02-12 22:30:54 +0700

    Update SpringredditclonerapApplication.java

--- src/main/java/com/rap/springredditclonerap/SpringredditclonerapApplication.java
+++ src/main/java/com/rap/springredditclonerap/SpringredditclonerapApplication.java
@@ -25,7 +25,7 @@ public class SpringredditclonerapApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://springreddit.bbjoo.com","http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://springangularreddit.bbjoo.com","http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
